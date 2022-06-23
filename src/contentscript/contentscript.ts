d18002442e6261e17d90114d1edca9b5107ec5ef
MartinPELCAT <martinpelcat@hotmail.com>
2022-06-23 20:51:33 +0200

    feat: remove sithy interface and auto collect twitch gifts automaticaly when a tab is open

--- src/contentscript/contentscript.ts
+++ src/contentscript/contentscript.ts
@@ -1,1 +1,21 @@
-import "./contentscript.scss";
+console.log("Auto collect is starting");
+
+const observer = new MutationObserver(function () {
+  const bonus = document.getElementsByClassName("claimable-bonus__icon");
+
+  if (bonus && bonus[0]) {
+    bonus[0].dispatchEvent(
+      new MouseEvent("click", {
+        bubbles: true,
+        cancelable: true,
+        view: window,
+      })
+    );
+  }
+});
+
+observer.observe(document, {
+  attributes: true,
+  childList: true,
+  subtree: true,
+});
