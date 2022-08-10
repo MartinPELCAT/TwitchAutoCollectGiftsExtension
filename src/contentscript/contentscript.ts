71dd5121779067e0d41633ac4ce7a56c4b9fffdd
MartinPELCAT <martinpelcat@hotmail.com>
2022-08-10 23:25:33 +0200

    change the code

--- src/contentscript/contentscript.ts
+++ src/contentscript/contentscript.ts
@@ -1,23 +1,45 @@
 console.log("Auto collect is starting");
 
-const chatInput = document.getElementsByClassName("chat-input").item(0);
+const waithForChatToAppear = (selector: string) =>
+  new Promise<Element>((resolve) => {
+    const foundObject = document.querySelectorAll(selector).item(0);
+    if (foundObject) return resolve(foundObject);
 
-const observer = new MutationObserver(function () {
-  const bonus = document.getElementsByClassName("claimable-bonus__icon");
+    const observer = new MutationObserver(function () {
+      const foundObject = document.querySelectorAll(selector).item(0);
+      if (foundObject) {
+        observer.disconnect();
+        return resolve(foundObject);
+      }
+    });
 
-  if (bonus && bonus[0]) {
-    bonus[0].dispatchEvent(
+    observer.observe(document, {
+      childList: true,
+      subtree: true,
+    });
+  });
+
+const setupBonusChestObserver = (containerToWatch: Element) => {
+  const observer = new MutationObserver(function () {
+    const bonus = document
+      .getElementsByClassName("claimable-bonus__icon")
+      .item(0);
+
+    bonus?.dispatchEvent(
       new MouseEvent("click", {
         bubbles: true,
         cancelable: true,
         view: window,
       })
     );
-  }
-});
+  });
 
-observer.observe(chatInput, {
-  attributes: true,
-  childList: true,
-  subtree: true,
+  observer.observe(containerToWatch, {
+    childList: true,
+    subtree: true,
+  });
+};
+
+waithForChatToAppear(".chat-input").then((chatInput) => {
+  setupBonusChestObserver(chatInput);
 });
